58729bd2fa59ceca5f979e52cb9d88df8f1c0b15
rathoddipak <dipakrathod23797@gmail.com>
2019-02-05 15:55:50 +0530

    CR changes in hamming.rb

--- hamming/hamming.rb
+++ hamming/hamming.rb
@@ -1,14 +1,6 @@
 #
 class Hamming
-  attr_accessor :string1, :string2
-  def initialize
-    @string1 = ''
-    @string2 = ''
-  end
-
   def self.compute(string1, string2)
-    string1 = string1
-    string2 = string2
     raise ArgumentError if string1.length != string2.length
     string1.chars.zip(string2.chars).count { |a, b| a != b }
   end
